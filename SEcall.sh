7aff696cb02ee2e2798e576fd5e741eedd2e0d47
ly178574378 <ly178574378@hotmail.com>
2017-10-27 15:22:33 -0400

    Create SEcall.sh

---
+++ SEcall.sh
@@ -0,0 +1,13 @@
+<SE_call.sh>
+#install ROSE
+wget https://bitbucket.org/young_computation/rose/get/1a9bb86b5464.zip
+unzip 1a9bb86b5464.zip
+rename $1 rose $1
+#select your species gff from ucsc 
+cp $genome_gff $rose/annotation/|chmod 777 
+#add your species into ROSE_main.py
+genomeDict = "$species"..........
+#make your enhancer bedfile into GFF
+awk 'BEGIN {FS="\t" ; OFS="\t"}{print $1,enhancer_"NR",$2,$3,".",".",".",".","NR"}'  your_enhancer.bed >your_enhancer.gff
+#Let's go
+python ROSE_main.py -g "$species" -i your_enhancer.gff -r your_IP.bam -o your_out_put -c your Input.bam
